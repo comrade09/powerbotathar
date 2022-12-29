0db2b19ef0898a5ee796da7da8b7d326ab3b7e0e
comrade09 <106185020+comrade09@users.noreply.github.com>
2022-12-29 14:19:11 +0530

    Update start.py

--- Shikimori/modules/start.py
+++ Shikimori/modules/start.py
@@ -46,9 +46,8 @@ bot_name = f"{dispatcher.bot.first_name}"
 
 
 PM_START_TEXT = """
-\n◍ Hey There! [👩‍💼](https://telegra.ph/file/b2d554241222fa7ea16a5.jpg) 
-I'ᴍ ʀɪᴋᴋᴀ ᴛᴀᴋᴀɴᴀꜱʜɪ Fʀᴏᴍ 
-◍ I'ᴍ Hɪɢʜʟʏ Aᴅᴠᴀɴᴄᴇ Gʀᴏᴜᴘ Mᴀɴᴀɢᴇᴍᴇɴᴛ Bᴏᴛ 
+\n I am Power(https://telegra.ph/file/b2d554241222fa7ea16a5.jpg) 
+I am an Anime themed group management with some fun features.
 ────────────────────────
 × Uᴘᴛɪᴍᴇ: {}
 ────────────────────────
